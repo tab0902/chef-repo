aa6f99fbcb543964a722fde7f40077049376c0ca
tab0902 <mercury.brave.0902@gmail.com>
2018-03-21 11:26:12 +0900

    Add Pillow to django dependencies

--- site-cookbooks/django/attributes/default.rb
+++ site-cookbooks/django/attributes/default.rb
@@ -1,1 +1,1 @@
-default['django']['packages'] = %w{ django mysqlclient pymysql twitter paypalrestsdk social-auth-app-django bottlenose beautifulsoup4 google-cloud-translate }
+default['django']['packages'] = %w{ django mysqlclient pymysql Pillow twitter paypalrestsdk social-auth-app-django bottlenose beautifulsoup4 google-cloud-translate }
