182d5028e513d924bcf56ff8bd239366adf88959
tab0902 <mercury.brave.0902@gmail.com>
2018-03-23 11:36:48 +0900

    Add cookbooks to create local environment for machine learning

---
+++ site-cookbooks/conda/README.md
@@ -0,0 +1,4 @@
+# conda
+
+TODO: Enter the cookbook description here.
+
---
+++ site-cookbooks/conda/recipes/default.rb
@@ -0,0 +1,26 @@
+#
+# Cookbook:: conda
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
+
+user_name         = node['user']['name']
+miniconda_version = node['miniconda']['version']
+python_version    = node['miniconda']['python']['version']
+packages          = node['conda']['packages']
+
+miniconda     = "/home/#{user_name}/.pyenv/versions/miniconda3-#{miniconda_version}"
+conda         = "#{miniconda}/bin/conda"
+site_packages = "#{miniconda}/lib/python#{python_version}/site-packages"
+
+packages.each do |key, value|
+  execute "install_#{key}" do
+    user "#{user_name}"
+    group "#{user_name}"
+    environment "HOME" => "/home/#{user_name}"
+    not_if "find #{site_packages}/#{value[:dir]}"
+    command <<-EOS
+      #{conda} install -y #{value[:pkg]}
+    EOS
+  end
+end
