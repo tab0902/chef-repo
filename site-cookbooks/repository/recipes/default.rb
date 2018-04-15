3173aee0c699057e673a0526e8cce93cc762d05f
tab0902 <mercury.brave.0902@gmail.com>
2018-04-15 16:24:34 +0900

    Revert "Remove old repository 'anybirth'"
    This reverts commit 5e4f3ace888ec181823a4ba7aa8cf22889aadcad.

--- site-cookbooks/repository/recipes/default.rb
+++ site-cookbooks/repository/recipes/default.rb
@@ -17,10 +17,6 @@ git "/home/#{user_name}/#{repo_name}" do
   notifies :run, "execute[remote_set_url]", :immediately
 end
 
-file "/home/#{user_name}/anybirth" do
-  action :delete
-end
-
 execute "remote_set_url" do
   action :nothing
   user "#{user_name}"
