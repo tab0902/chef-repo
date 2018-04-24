56b7ef0850af3afbdc17fccfcdbd87df3f83b1b3
tab0902 <mercury.brave.0902@gmail.com>
2018-04-24 17:12:12 +0900

    Add VirtualHost settings

--- site-cookbooks/mod_wsgi/attributes/default.rb
+++ site-cookbooks/mod_wsgi/attributes/default.rb
@@ -1,2 +1,2 @@
-default['mod_wsgi']['wsgi_conf'] = "/etc/httpd/conf.d/wsgi.conf"
 default['mod_wsgi']['python_conf'] = "/etc/httpd/conf.d/python.conf"
+default['mod_wsgi']['virtual_conf'] = "/etc/httpd/conf.d/virtual.conf"
--- site-cookbooks/mod_wsgi/recipes/default.rb
+++ site-cookbooks/mod_wsgi/recipes/default.rb
@@ -4,37 +4,44 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
-wsgi_conf         = node['mod_wsgi']['wsgi_conf']
 python_conf       = node['mod_wsgi']['python_conf']
+virtual_conf      = node['mod_wsgi']['virtual_conf']
 user_name         = node['user']['name']
 project_name      = node['repository']['name']
+domain            = node['certbot']['domain']
 miniconda_version = node['miniconda']['version']
 python_version    = node['miniconda']['python']['version']
 
+certbot  = "/home/#{user_name}/certbot"
 pip      = "/home/#{user_name}/.pyenv/versions/miniconda3-#{miniconda_version}/bin/pip"
 mod_wsgi = "/home/#{user_name}/.pyenv/versions/miniconda3-#{miniconda_version}/lib/python#{python_version}/site-packages/mod_wsgi"
-
-template "#{wsgi_conf}" do
-  owner "root"
-  group "root"
-  mode 0644
-  source "wsgi.conf.erb"
-  variables({
-    :user_name => user_name,
-    :python_version => python_version,
-    :miniconda_version => miniconda_version
-  })
-end
 
 template "#{python_conf}" do
   owner "root"
   group "root"
   mode 0644
   source "python.conf.erb"
+  not_if "find #{certbot}"
   variables({
     :user_name => user_name,
     :miniconda_version => miniconda_version,
+    :python_version => python_version,
     :project_name => project_name
+  })
+end
+
+template "#{virtual_conf}" do
+  owner "root"
+  group "root"
+  mode 0644
+  source "virtual.conf.erb"
+  only_if "find #{certbot}"
+  variables({
+    :user_name => user_name,
+    :miniconda_version => miniconda_version,
+    :python_version => python_version,
+    :project_name => project_name,
+    :domain => domain
   })
 end
 
