76a8a5d226e160b4ad61275afcec270e8699be9a
tab0902 <mercury.brave.0902@gmail.com>
2018-06-23 20:23:56 +0900

    Move attributes to roles and update some of them

--- site-cookbooks/git/attributes/default.rb
+++ site-cookbooks/git/attributes/default.rb
@@ -1,4 +1,4 @@
-default['git']['version']    = "2.16.2"
+default['git']['version']    = "2.18.0"
 default['git']['source_uri'] = "https://www.kernel.org/pub/software/scm/git/git-#{default['git']['version']}.tar.gz"
 default['git']['install_dir'] = "/usr/local/src"
 default['git']['packages']   = %w{ libcurl-devel expat-devel gettext gettext-devel openssl-devel perl-devel zlib-devel gcc perl-ExtUtils-MakeMaker }
