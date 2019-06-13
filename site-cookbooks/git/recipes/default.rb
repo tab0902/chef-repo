b376e08b09e6a6bb6a262d03f233cb97334eb19c
tab0902 <mercury.brave.0902@gmail.com>
2019-06-14 02:30:57 +0900

    Make sure that designated version of git is installed

--- site-cookbooks/git/recipes/default.rb
+++ site-cookbooks/git/recipes/default.rb
@@ -16,14 +16,13 @@ node['git']['packages'].each do |item|
 end
 
 remote_file "#{Chef::Config[:file_cache_path]}/git-#{version}.tar.gz" do
-  not_if "which git"
   source "#{source_uri}"
 end
 
 execute "install_git" do
   user "root"
   group "root"
-  not_if "which git"
+  not_if "find #{install_dir}/git-#{version}"
   command <<-EOS
     cd #{install_dir}
     tar xfz #{Chef::Config[:file_cache_path]}/git-#{version}.tar.gz -C #{install_dir}
@@ -33,14 +32,13 @@ execute "install_git" do
     make all
     make install
   EOS
-  notifies :run, "execute[git_settings]", :immediately
 end
 
 execute "git_settings" do
-  action :nothing
   user "#{user_name}"
   group "#{user_name}"
   environment "HOME" => "/home/#{user_name}"
+  not_if "find /home/#{user_name}/.ssh/id_rsa"
   command <<-EOS
     git config --global merge.ff false
     git config --global pull.ff only
