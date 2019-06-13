3d97af0ea4c77c09c59651ec6f1a4f87c8888272
tab0902 <mercury.brave.0902@gmail.com>
2019-06-14 01:28:01 +0900

    Add php-xml to PHP packages

--- site-cookbooks/php/attributes/default.rb
+++ site-cookbooks/php/attributes/default.rb
@@ -1,5 +1,5 @@
 default['php']['version'] = "72"
-default['php']['packages'] = %w{ php php-devel php-mysqlnd php-pdo php-gd php-mbstring php-opcache php-pecl-xdebug }
+default['php']['packages'] = %w{ php php-devel php-mysqlnd php-pdo php-gd php-mbstring php-opcache php-xml php-pecl-xdebug }
 default['php']['ini'] = "/etc/php.ini"
 default['php']['timezone'] = "Asia/Tokyo"
 default['php']['language'] = "Japanese"
