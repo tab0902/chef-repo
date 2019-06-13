dc5da7e17b2f97b1db9d340a8b7ffe28e5deb34a
tab0902 <mercury.brave.0902@gmail.com>
2019-06-14 01:58:48 +0900

    Add cookbook 'cakephp'

---
+++ site-cookbooks/cakephp/README.md
@@ -0,0 +1,4 @@
+# cakephp
+
+TODO: Enter the cookbook description here.
+
---
+++ site-cookbooks/cakephp/recipes/default.rb
@@ -0,0 +1,31 @@
+#
+# Cookbook:: cakephp
+# Recipe:: default
+#
+# Copyright:: 2019, The Authors, All Rights Reserved.
+
+version       = node['php']['version']
+user_name     = node['user']['name']
+repositories  = node['repository']['repositories']
+composer_dir  = node['composer']['install_dir']
+composer_file = node['composer']['file_name']
+
+composer = "#{composer_dir}/#{composer_file}"
+
+package "php-intl" do
+  action [ :install, :upgrade ]
+  options "--enablerepo=remi,remi-php#{version}"
+end
+
+repositories.each do |repository|
+  repo_name = repository['name']
+
+  execute "install_cakephp_files_to_#{repo_name}" do
+    user "#{user_name}"
+    group "#{user_name}"
+    cwd "/home/#{user_name}/#{repo_name}"
+    command <<-EOS
+      #{composer} install -n
+    EOS
+  end
+end
