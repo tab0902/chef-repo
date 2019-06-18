e0f4a4364c6244da2907c1f952efe6197a341e3e
tab0902 <mercury.brave.0902@gmail.com>
2019-06-18 11:10:30 +0000

    Add glibc-headers to default['git']['packages']

--- site-cookbooks/git/attributes/default.rb
+++ site-cookbooks/git/attributes/default.rb
@@ -1,3 +1,3 @@
 default['git']['source_uri'] = "https://www.kernel.org/pub/software/scm/git/git-#{node['git']['version']}.tar.gz"
 default['git']['install_dir'] = "/usr/local"
-default['git']['packages'] = %w{ libcurl-devel expat-devel gettext-devel openssl-devel perl-devel zlib-devel gcc autoconf perl-ExtUtils-MakeMaker }
+default['git']['packages'] = %w{ libcurl-devel expat-devel gettext-devel openssl-devel perl-devel zlib-devel gcc autoconf perl-ExtUtils-MakeMaker glibc-headers }
