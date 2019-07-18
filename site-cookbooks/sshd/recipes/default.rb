83c6acdfb712faa6e7286ec749e34fcd778b8cec
tab0902 <tabuchi@ewewing.com>
2019-07-19 02:05:04 +0900

    Update sshd_config

--- site-cookbooks/sshd/recipes/default.rb
+++ site-cookbooks/sshd/recipes/default.rb
@@ -9,13 +9,18 @@ environment = node.chef_environment
 data_bag = Chef::EncryptedDataBagItem.load('sshd','port')
 port     = data_bag["#{environment}"]
 
-execute 'disable_selinux' do
+execute 'edit_sshd_config' do
   user 'root'
   group 'root'
   not_if "grep -q 'Port #{port}' /etc/ssh/sshd_config"
   command <<-EOS
     cp -p /etc/ssh/sshd_config /etc/ssh/sshd_config.org
-    sed -i -e "s/#Port 22/Port #{port}/g" /etc/ssh/sshd_config
+    sed -i -e "s/Port 22/Port #{port}/g" /etc/ssh/sshd_config
+    sed -i -e "s/#Port #{port}/Port #{port}/g" /etc/ssh/sshd_config
+    sed -i -e "s/PermitRootLogin yes/PermitRootLogin no/g" /etc/ssh/sshd_config
+    sed -i -e "s/#PermitRootLogin no/PermitRootLogin no/g" /etc/ssh/sshd_config
+    sed -i -e "s/PasswordAuthentication yes/PasswordAuthentication no/g" /etc/ssh/sshd_config
+    sed -i -e "s/#PasswordAuthentication no/PasswordAuthentication no/g" /etc/ssh/sshd_config
   EOS
   notifies :restart, 'service[sshd]', :delayed
 end
