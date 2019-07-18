fe11b8cc95d05748f9b139b6c0d103493fc11cc1
tab0902 <tabuchi@ewewing.com>
2019-07-18 12:44:34 +0900

    Make it possible to define variables in node['mod_wsgi']

--- site-cookbooks/mod_wsgi/attributes/default.rb
+++ site-cookbooks/mod_wsgi/attributes/default.rb
@@ -1,2 +1,1 @@
 default['mod_wsgi']['wsgi_conf'] = "/etc/httpd/conf.d/wsgi.conf"
-default['mod_wsgi']['vhost_conf'] = "/etc/httpd/conf.d/vhost.conf"
--- site-cookbooks/mod_wsgi/recipes/default.rb
+++ site-cookbooks/mod_wsgi/recipes/default.rb
@@ -5,10 +5,9 @@
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
 wsgi_conf         = node['mod_wsgi']['wsgi_conf']
-vhost_conf        = node['mod_wsgi']['vhost_conf']
+ports             = node['mod_wsgi']['ports']
+projects          = node['mod_wsgi']['projects']
 user_name         = node['user']['name']
-projects          = node['repository']['repositories']
-domains           = node['certbot']['domains']
 miniconda_version = node['miniconda']['version']
 python_version    = node['miniconda']['python']['version']
 
@@ -16,37 +15,17 @@ certbot  = "/home/#{user_name}/certbot"
 pip      = "/home/#{user_name}/.pyenv/versions/miniconda#{python_version.to_i}-#{miniconda_version}/bin/pip"
 mod_wsgi = "/home/#{user_name}/.pyenv/versions/miniconda#{python_version.to_i}-#{miniconda_version}/lib/python#{python_version}/site-packages/mod_wsgi"
 
-project_names = []
-projects.each do |project|
-  project_names.push(project['name'])
-end
-
 template "#{wsgi_conf}" do
   owner "root"
   group "root"
   mode 0644
   source "wsgi.conf.erb"
-  not_if "find #{certbot}"
   variables({
     :user_name => user_name,
     :miniconda_version => miniconda_version,
     :python_version => python_version,
-    :project_name => project_names[0]
-  })
-end
-
-template "#{vhost_conf}" do
-  owner "root"
-  group "root"
-  mode 0644
-  source "vhost.conf.erb"
-  only_if "find #{certbot}"
-  variables({
-    :user_name => user_name,
-    :miniconda_version => miniconda_version,
-    :python_version => python_version,
-    :project_names => project_names,
-    :domains => domains
+    :ports => ports,
+    :projects => projects
   })
 end
 
