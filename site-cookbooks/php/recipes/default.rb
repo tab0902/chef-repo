b531179f1a59ad8baa209b6c453bc044622e52f6
tab0902 <tabuchi@ewewing.com>
2019-08-02 23:07:28 +0900

    Use resource execute instead of template in order to edit php.ini

--- site-cookbooks/php/recipes/default.rb
+++ site-cookbooks/php/recipes/default.rb
@@ -19,16 +19,34 @@ packages.each do |item|
   end
 end
 
-template "#{php_ini}" do
-  owner "root"
-  group "root"
-  mode 0644
-  source 'php.ini.erb'
+execute 'edit_php_ini' do
+  user 'root'
+  group 'root'
+  not_if "grep -q 'mbstring.internal_encoding = #{encoding}' #{php_ini}"
+  command <<-EOS
+    cp -p #{php_ini} #{php_ini}.org
+    sed -i -e "s/;date.timezone =/date.timezone = '#{timezone}'/g" #{php_ini}
+    sed -i -e "s/;mbstring.language = Japanese/mbstring.language = #{language}/g" #{php_ini}
+    sed -i -e "s/;mbstring.internal_encoding =/mbstring.internal_encoding = #{encoding}/g" #{php_ini}
+    sed -i -e "s/;mbstring.http_input =/mbstring.http_input = #{encoding}/g" #{php_ini}
+  EOS
   notifies :restart, 'service[httpd]', :delayed
-  variables({
-    :timezone => timezone,
-    :language => language,
-    :encoding => encoding,
-    :xdebug => xdebug,
-  })
+end
+
+if node['php']['packages'].include?("php-pecl-xdebug") then
+  execute "add_settings_for_xdebug" do
+    user "root"
+    group "root"
+    not_if "grep -q 'xdebug.coverage_enable=1' #{php_ini}"
+    command <<-EOS
+      echo 'xdebug.coverage_enable=1' >> #{php_ini}
+      echo 'xdebug.default_enable=1' >> #{php_ini}
+      echo 'xdebug.profiler_enable=1' >> #{php_ini}
+      echo 'xdebug.profiler_output_dir="/tmp"' >> #{php_ini}
+      echo 'xdebug.remote_autostart=1' >> #{php_ini}
+      echo 'xdebug.remote_enable=1' >> #{php_ini}
+      echo 'xdebug.remote_host=localhost' >> #{php_ini}
+      echo 'xdebug.remote_port=9000' >> #{php_ini}
+    EOS
+  end
 end
