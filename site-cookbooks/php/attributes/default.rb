726569634eaa749437cc9af150d8ed27bae6ba6d
tab0902 <tabuchi@ewewing.com>
2019-08-03 14:13:29 +0900

    Make it possible to define variables in node['php']

--- site-cookbooks/php/attributes/default.rb
+++ site-cookbooks/php/attributes/default.rb
@@ -1,5 +1,1 @@
-default['php']['packages'] = %w{ php php-devel php-mysqlnd php-pdo php-gd php-mbstring php-opcache php-xml php-pecl-xdebug }
 default['php']['ini'] = "/etc/php.ini"
-default['php']['timezone'] = "Asia/Tokyo"
-default['php']['language'] = "Japanese"
-default['php']['encoding'] = "UTF-8"
