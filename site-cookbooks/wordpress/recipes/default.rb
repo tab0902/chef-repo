1ea458af71ab605bf0cf65034b2fc1299338f1c5
tab0902 <tabuchi@ewewing.com>
2019-08-03 14:03:44 +0900

    Update recipe of wordpress

--- site-cookbooks/wordpress/recipes/default.rb
+++ site-cookbooks/wordpress/recipes/default.rb
@@ -9,56 +9,73 @@ db_host    = node['mysql']['db_host']
 charset    = node['mysql']['config']['charset']
 collate    = node['mysql']['config']['collate']
 repository = node['wordpress']['repository']
-db_name    = node["wordpress"]["db_name"]
+projects   = node["wordpress"]["projects"]
 environment = node.chef_environment
 
-wordpress = "/home/#{user_name}/wordpress"
 
-remote_file "/home/#{user_name}/#{repository}" do
-  owner "#{user_name}"
-  group "#{user_name}"
-  mode 0644
-  source "http://ja.wordpress.org/#{repository}"
-end
+projects.each do |key, project|
 
-execute "unzip_wordpress" do
-  user "#{user_name}"
-  group "#{user_name}"
-  cwd "/home/#{user_name}"
-  not_if "find #{wordpress}"
-  command <<-EOS
-    tar xfz /home/#{user_name}/#{repository}
-  EOS
-end
+  source      = "/home/#{user_name}/#{repository}"
+  destination = "/home/#{user_name}#{project['destination']}"
+  directory   = "#{project['directory']}"
+  db_name     = "#{project['db_name']}"
 
-execute "chmod_wp-content" do
-  user "#{user_name}"
-  group "#{user_name}"
-  not_if "test `stat -c '%a' /home/#{user_name}/wordpress/wp-content` -eq '777'"
-  command <<-EOS
-    chmod -R 777 /home/#{user_name}/wordpress/wp-content
-  EOS
-end
+  remote_file "#{source}" do
+    owner "#{user_name}"
+    group "#{user_name}"
+    mode 0644
+    source "http://ja.wordpress.org/#{repository}"
+    not_if "find #{destination}/#{directory}"
+  end
 
-file "/home/#{user_name}/#{repository}" do
-  action :delete
-end
+  execute "unzip_wordpress" do
+    user "#{user_name}"
+    group "#{user_name}"
+    cwd "/home/#{user_name}"
+    not_if "find #{destination}/#{directory}"
+    command <<-EOS
+      tar xfz #{source}
+    EOS
+  end
 
-data_bag = Chef::EncryptedDataBagItem.load('passwords','mysql')
-password = data_bag["#{environment}"]
+  file "#{source}" do
+    action :delete
+  end
 
-template "#{wordpress}/wp-config.php" do
-  owner "#{user_name}"
-  group "#{user_name}"
-  mode 0644
-  source "wp-config.php.erb"
-  action :create_if_missing
-  variables({
-    :db_name => db_name,
-    :user_name => user_name,
-    :password => password,
-    :db_host => db_host,
-    :charset => charset,
-    :collate => collate
-  })
+  execute "rename_directory" do
+    user "#{user_name}"
+    group "#{user_name}"
+    not_if "find #{destination}/#{directory}"
+    command <<-EOS
+      mv #{destination}/wordpress #{destination}/#{directory}
+    EOS
+  end
+
+  execute "chmod_wp-content" do
+    user "#{user_name}"
+    group "#{user_name}"
+    not_if "test `stat -c '%a' #{destination}/#{directory}/wp-content` -eq '777'"
+    command <<-EOS
+      chmod -R 777 #{destination}/#{directory}/wp-content
+    EOS
+  end
+
+  data_bag = Chef::EncryptedDataBagItem.load('passwords','mysql')
+  password = data_bag["#{environment}"]
+
+  template "#{destination}/#{directory}/wp-config.php" do
+    owner "#{user_name}"
+    group "#{user_name}"
+    mode 0644
+    source "wp-config.php.erb"
+    action :create_if_missing
+    variables({
+      :db_name => db_name,
+      :user_name => user_name,
+      :password => password,
+      :db_host => db_host,
+      :charset => charset,
+      :collate => collate
+    })
+  end
 end
